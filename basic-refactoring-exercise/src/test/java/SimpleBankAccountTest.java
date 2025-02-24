93098c0e98f8346457f45cc3f6cec15981745384
noodles <elvis.perlika@studio.unibo.it>
2025-02-24 10:41:55 +0100

    refactor: move magic numbers inside methods

--- basic-refactoring-exercise/src/test/java/SimpleBankAccountTest.java
+++ basic-refactoring-exercise/src/test/java/SimpleBankAccountTest.java
@@ -10,18 +10,19 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class SimpleBankAccountTest {
 
-    public static final int AMOUNT_100 = 100;
-    public static final int AMOUNT_50 = 50;
-    public static final int AMOUNT_70 = 70;
-    public static final int EXPECTED_30 = 30;
     private static final double WITHDRAW_FEE = 1;
     private AccountHolder accountHolder;
+    private AccountHolder accountHolder2;
     private BankAccount bankAccount;
+    private BankAccount bankAccount2;
 
     @BeforeEach
     void beforeEach(){
         accountHolder = new AccountHolder("Mario", "Rossi", 1);
         bankAccount = new SimpleBankAccount(accountHolder, 0);
+
+        accountHolder2 = new AccountHolder("Giovanni", "Rana", 2);
+        bankAccount2 = new SimpleBankAccount(accountHolder2, 0);
     }
 
     @Test
@@ -31,36 +32,49 @@ class SimpleBankAccountTest {
 
     @Test
     void testDeposit() {
-        bankAccount.deposit(accountHolder.getId(), AMOUNT_100);
-        assertEquals(AMOUNT_100, bankAccount.getBalance());
+        final int amount = 100;
+        final int expected = 100;
+        bankAccount.deposit(accountHolder.getId(), amount);
+        assertEquals(expected, bankAccount.getBalance());
     }
 
     @Test
     void testWrongDeposit() {
-        bankAccount.deposit(accountHolder.getId(), AMOUNT_100);
-        bankAccount.deposit(2, AMOUNT_50);
-        assertEquals(AMOUNT_100, bankAccount.getBalance());
+        final int amountForAccount1 = 100;
+        final int amountForAccount2 = 50;
+        final int expected = 100;
+        bankAccount.deposit(accountHolder.getId(), amountForAccount1);
+        bankAccount.deposit(2, amountForAccount2);
+        assertEquals(expected, bankAccount.getBalance());
     }
 
     @Test
     void testWithdraw() {
-        bankAccount.deposit(accountHolder.getId(), AMOUNT_100);
-        bankAccount.withdraw(accountHolder.getId(), AMOUNT_70);
-        assertEquals(EXPECTED_30, bankAccount.getBalance());
+        final int amount = 100;
+        final int withdrawAmount = 70;
+        final int expected = 30;
+        bankAccount.deposit(accountHolder.getId(), amount);
+        bankAccount.withdraw(accountHolder.getId(), withdrawAmount);
+        assertEquals(expected, bankAccount.getBalance());
     }
 
     @Test
     void testWrongWithdraw() {
-        bankAccount.deposit(accountHolder.getId(), AMOUNT_100);
-        bankAccount.withdraw(2, AMOUNT_70);
-        assertEquals(AMOUNT_100, bankAccount.getBalance());
+        final int amount = 100;
+        final int expected = 100;
+        int withdrawAmount = 70;
+        bankAccount.deposit(accountHolder.getId(), amount);
+        bankAccount.withdraw(accountHolder2.getId(), withdrawAmount);
+        assertEquals(expected, bankAccount.getBalance());
     }
 
     @Test
     void testFeeWhenWithdraw() {
+        final int amount = 100;
+        final int withdrawAmount = 100;
         double previousBalance = bankAccount.getBalance();
-        bankAccount.deposit(accountHolder.getId(), AMOUNT_100);
-        bankAccount.withdrawWithFee(accountHolder.getId(), AMOUNT_100);
+        bankAccount.deposit(accountHolder.getId(), amount);
+        bankAccount.withdrawWithFee(accountHolder.getId(), withdrawAmount);
         assertEquals(bankAccount.getBalance(), previousBalance - WITHDRAW_FEE);
     }
 }
